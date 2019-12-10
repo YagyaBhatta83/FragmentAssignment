39f57d15703c74454ab58823c9ceb260748c10db
YagyaBhatta83 <bhattayagya123@gmail.com>
2019-12-10 19:52:10 +0545

    Finally all swap Number is also checked sucessfully.

--- app/src/main/java/com/yagya/fragmentassignment/fragments/SwapNumberFragment.java
+++ app/src/main/java/com/yagya/fragmentassignment/fragments/SwapNumberFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.yagya.fragmentassignment.R;
 
@@ -18,7 +19,7 @@ import com.yagya.fragmentassignment.R;
  */
 public class SwapNumberFragment extends Fragment implements View.OnClickListener{
     private Button btncalcswap;
-    private EditText etnumber;
+    private EditText etnumber1, etnumber2;
 
 
     public SwapNumberFragment() {
@@ -31,9 +32,9 @@ public class SwapNumberFragment extends Fragment implements View.OnClickListener
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_swap_number, container, false);
-        etnumber = view.findViewById(R.id.etnumber);
+        etnumber1 = view.findViewById(R.id.etnumber1);
+        etnumber2 = view.findViewById(R.id.etnumber2);
         btncalcswap = view.findViewById(R.id.btncalcswap);
-
         btncalcswap.setOnClickListener(this);
 
         return view;
@@ -41,6 +42,18 @@ public class SwapNumberFragment extends Fragment implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
+        int num1 = Integer.parseInt(etnumber1.getText().toString());
+        int num2 = Integer.parseInt(etnumber2.getText().toString());
+        int a=10, b=20;
+        num1=num1+num2;//a=30 (10+20)
+        num2=num1-num2;//b=10 (30-20)
+        num1=num1-num2;//a=20 (30-10)
+
+        if (num1==num2){
+            Toast.makeText(getActivity(),"The number is a Swap number", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(getActivity(),"The number is not a Swap number", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
