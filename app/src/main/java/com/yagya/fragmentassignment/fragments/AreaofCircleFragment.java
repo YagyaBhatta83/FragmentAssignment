9c6d592c0298e2f9f8e16fd3b7025186bff44e36
YagyaBhatta83 <bhattayagya123@gmail.com>
2019-12-10 11:10:55 +0545

    Area of circle calculated sucessfully.

--- app/src/main/java/com/yagya/fragmentassignment/fragments/AreaofCircleFragment.java
+++ app/src/main/java/com/yagya/fragmentassignment/fragments/AreaofCircleFragment.java
@@ -8,13 +8,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.yagya.fragmentassignment.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AreaofCircleFragment extends Fragment {
+public class AreaofCircleFragment extends Fragment implements View.OnClickListener {
+    private Button btnareaofcircle;
+    private EditText etRadius;
 
 
     public AreaofCircleFragment() {
@@ -26,7 +31,23 @@ public class AreaofCircleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_areaof_circle, container, false);
+        View view = inflater.inflate(R.layout.fragment_areaof_circle, container, false);
+
+        etRadius = view.findViewById(R.id.etRadius);
+        btnareaofcircle = view.findViewById(R.id.btnareaofcircle);
+
+        btnareaofcircle.setOnClickListener(this);
+
+        return view;
     }
 
+
+    @Override
+    public void onClick(View v) {
+        float radius = Float.parseFloat(etRadius.getText().toString());
+        float area = 3.143f * radius * radius;
+
+        Toast.makeText(getActivity(),"Area of Circle is : " + area, Toast.LENGTH_SHORT).show();
+
+    }
 }
